0adef50d39a764b60739ad2d6cb5c01dd962d96d
Derek Shashek <dshashek@gmail.com>
2019-06-20 18:06:32 -0500

    fixed SQL homework, replaced bad image, redid unsaved schema changes

--- schema.sql
+++ schema.sql
@@ -4,20 +4,13 @@
 
 CREATE TABLE "employees" (
     "emp_no" int   NOT NULL,
+    "birth_date" date   NOT NULL,
     "first_name" varchar(255)   NOT NULL,
     "last_name" varchar(255)   NOT NULL,
     "gender" varchar(255)   NOT NULL,
     "hire_date" date   NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
-     )
-);
-
-CREATE TABLE "departments" (
-    "dept_no" varchar(255)   NOT NULL,
-    "dept_name" varchar(255)   NOT NULL,
-    CONSTRAINT "pk_departments" PRIMARY KEY (
-        "dept_no"
      )
 );
 
@@ -28,6 +21,14 @@ CREATE TABLE "dept_manager" (
     "to_date" date   NOT NULL
 );
 
+CREATE TABLE "departments" (
+    "dept_no" varchar(255)   NOT NULL,
+    "dept_name" varchar(255)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
 CREATE TABLE "dept_emp" (
     "emp_no" int   NOT NULL,
     "dept_no" varchar(255)   NOT NULL,
@@ -35,15 +36,16 @@ CREATE TABLE "dept_emp" (
     "to_date" date   NOT NULL
 );
 
-CREATE TABLE "Salaries" (
+CREATE TABLE "salaries" (
     "emp_no" int   NOT NULL,
-    "Salary" int   NOT NULL,
+    "salary" int   NOT NULL,
     "from_date" date   NOT NULL,
     "to_date" date   NOT NULL
 );
 
-CREATE TABLE "Titles" (
+CREATE TABLE "titles" (
     "emp_no" int   NOT NULL,
+    "title" varchar(255)   NOT NULL,
     "from_date" date   NOT NULL,
     "to_date" date   NOT NULL
 );
@@ -60,9 +62,9 @@ REFERENCES "employees" ("emp_no");
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "departments" ("dept_no");
 
-ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_emp_no" FOREIGN KEY("emp_no")
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
